b8dcba582d592cf895ef1c92eae1801fa00446cd
muayyadayesh <muayyadayesh@gmail.com>
2021-04-16 22:59:59 +0300

    Initial Commit

---
+++ ImagePreviewer/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ImagePreviewer
+//
+//  Created by mac book pro on 4/16/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ ImagePreviewer/ImagePreviewerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ImagePreviewerApp.swift
+//  ImagePreviewer
+//
+//  Created by mac book pro on 4/16/21.
+//
+
+import SwiftUI
+
+@main
+struct ImagePreviewerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
